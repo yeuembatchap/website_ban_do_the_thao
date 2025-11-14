d17ccf2b1f6c63565b4b5f57c5a8ed0bc649a7fc
yeuembatchap <doideplai@gmail.com>
2025-11-14 09:58:45 +0700

    them index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+</head>
+<body>
+    Đây là trang chủ của cửa hàng bán đồ thể thao.
+</body>
+</html>
